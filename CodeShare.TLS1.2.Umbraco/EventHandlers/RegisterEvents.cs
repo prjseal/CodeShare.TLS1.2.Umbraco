b60dbcae42e49140272709ce402854b24cf5d0c6
Paul Seal <prjseal@gmail.com>
2018-11-24 23:45:48 +0000

    - first commits

---
+++ CodeShare.TLS1.2.Umbraco/EventHandlers/RegisterEvents.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Core;
+
+namespace CodeShare.TLS1._2.Umbraco.EventHandlers
+{
+    public class RegisterEvents : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CodeShare.TLS1.2.Umbraco
 A simple package which sets the security protocol in your Umbraco application to TLS 1.2
+
+[![Build status](https://ci.appveyor.com/api/projects/status/18bh0sg0x1wscbkr?svg=true)](https://ci.appveyor.com/project/prjseal/codeshare-tls1-2-umbraco)
